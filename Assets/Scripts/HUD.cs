412890de035454cdfefdf57d55ab06569fb3dddd
Nexe <rohtirautela1408@gmail.com>
2022-03-20 14:57:32 +0530

    Added score system without its functionality

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HUD : MonoBehaviour
+{
+    public void SetLeftPaddelScore(int value) {}
+    public void SetRightPaddelScore(int value) {}
+}
